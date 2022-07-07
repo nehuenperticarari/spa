21526c727ed915bbe97829653a95ba79d4d3d8cb
Nehuen Perticarari <nehuenperticarari@gmail.com>
2022-07-07 12:07:52 -0300

    second commit

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,2 @@
-var swiper=new Swiper(".swiper",{effect:"cube",grabCursor:!0,cubeEffect:{shadow:!0,slideShadows:!0,shadowOffset:20,shadowScale:.94}});
+var swiper=new Swiper(".swiper",{effect:"cube",grabCursor:!0,cubeEffect:{shadow:!0,slideShadows:!0,shadowOffset:20,shadowScale:.94},pagination:{el:".swiper-pagination"}});
 //# sourceMappingURL=main.js.map
